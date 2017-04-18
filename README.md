867114eee30fe55048d6ecbf476a36842a1df857
tanjo <tanjo@users.noreply.github.com>
2017-04-18 21:09:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# slack-police-unused-channel
+
+## Setup
+
+### Download
+
+```
+npm install
+```
+
+### token.json
+
+```
+{
+  "value": "ここにトークン"
+}
+```
+
+## Run
+
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var request = require('sync-request');
+var token = require('./token');
+
+console.log(token.value);
+
+fs.writeFileSync('./data.json', request('GET', "https://slack.com/api/channels.list?token=" + token.value + "&exclude_archived=true"));
+
+var data = require('./data.json');
+
+fs.writeFileSync('./result.txt', "" + data.channels.length + "\n");
+
+for (var i = 0; i < data.channels.length; i++) {
+  var channel = data.channels[i];
+  if (channel.is_channel) {
+    var url = "https://slack.com/api/channels.history?token=" + token.value + "&channel=" + channel.id;
+    var response = request('GET', url);
+    var body = JSON.parse(response.getBody().toString());
+    if (body && body.messages && body.messages.length !== 0) {
+      var message = body.messages[0];
+      if (message && message.type === "message") {
+        var d = new Date(message.ts * 1000);
+        var d20161231 = new Date("2016/12/31");
+        if (d.getTime() <= d20161231.getTime()) {
+          fs.appendFile('./result.txt', i + " #" + channel.name + " @" + d.toLocaleString() + "\n");
+        }
+      } else {
+        fs.appendFile('./result.txt', i + " #" + channel.name + " @UnknownType\n");
+      }
+    } else {
+      fs.appendFile('./result.txt', i + " #" + channel.name + " @NoContent\n");
+    }
+  }
+}
